8424bdc829483ea03ff92f6dc44444d9c7fc39a9
seniordesign2017 <e.spagnoletti@vikes.csuohio.edu>
2017-05-05 01:16:39 -0400

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 $(function() {
 	//REPLACE DEVICE UNIQUE IDENTIFIER / SERIAL NUMBER HERE
-	var myDevice = 'B4:21:8A:F0:2E:CE'; //default unique device identifier
+	var myDevice = 'B4:21:8A:F0:70:91';//var myDevice = 'B4:21:8A:F0:2E:CE'; //default unique device identifier  B4:21:8A:F0:36:58
 	//REPLACE WITH FULL APP DOMAIN IF RUNNING LOCALLY, OTHEWISE LEAVE AS "/"
 	var app_domain = '/';
 	var data = [];
@@ -9,7 +9,11 @@ $(function() {
 	var timeWindow = 10; //minutes
 	var red_color = '#6B0023';
 	var graphType = "all";
-
+	var graphIndex = 0;
+	var index = 1;
+	document.getElementById("sleeperDiv2").style.visibility = "hidden";
+	document.getElementById("sleeperDiv3").style.visibility = "hidden";
+    
     var graph_options = {
         series: {
             lines: { show: true, lineWidth: 1.5, fill: 0.1},
@@ -20,10 +24,6 @@ $(function() {
 			backgroundColor: "#111111",
 			backgroundOpacity: 0.8
 		},
-        yaxis: {
-			min: 0,
-			max: 400
-        },
         xaxis: {
 			mode: "time",
 			timeformat: "%I:%M %p",
@@ -33,26 +33,22 @@ $(function() {
         colors: ["#2C9DB6","#FF921E","#FF5847","#FFC647", "#5D409C", "#BF427B","#D5E04D" ]
 	};
 
+	//$("#graphPick").val( "temper" );
 	$("#specificdevice").text(myDevice);
 	$("#currentdevice").text(myDevice);
 	$("#appstatus").text('Running');
 	$("#appstatus").css('color', '555555');
-	$("#appconsole").text('starting...');
-	$("#appconsole").css('color', '#555555');
 	$("#placeholder").text('Graph: Retrieving Data Now....');
 
     function fetchData() {
+        updateValues();
 		
 		console.log('fetching data from Murano');
-        $("#appconsole").text('Fetching Data For '+myDevice+' From Server...');
-		$("#appconsole").css('color', '#555555');
 
         // recent data is grabbed as newdata
         function onDataReceived(newdata) {
 			$("#appstatus").text('Running');
 			$("#appstatus").css('color', '555555');
-			$("#appconsole").text('Processing Data');
-			$("#appconsole").css('color', '#555555');
 			var data_to_plot = [];
 			//Load all the data in one pass; if we only got partial
 			// data we could merge it with what we already have.
@@ -64,12 +60,12 @@ $(function() {
             //newdata has no data
             //Database error
             console.log('no data in selected window, check device')
-            $("#appconsole").text('No data found in window for this device');
             $("#placeholder").text('Graph: Data Not Found for: '+myDevice);
 			}else{
 				//newdata has data
 				console.log('valid data return for: '+myDevice);
 				//for each column in the newdata from timeseries 
+				
 				for (j = 1; j < newdata.timeseries.columns.length; j++){
 					var data = [];
 					//set data from newdata to raw_data
@@ -107,17 +103,19 @@ $(function() {
 						friendly = "Barometric Pressure";
 					
 					}
+					
 
 					console.log(raw_data, j);
 
 					// reformat data for flot
 					for (var i = raw_data.length - 1; i >= 0; i--) {
-						if (raw_data[i][j] != null)
-						data.unshift([raw_data[i][0],raw_data[i][j]])
+						if (raw_data[i][j] != null){
+							data.unshift([raw_data[i][0],raw_data[i][j]]);
+						}
 					}
-
+					
 					// only push if data returned
-					if(graphType == "all"||(graphType=="temper" && friendly == "Pump Temperature")||(graphType=="press" && friendly == "Pressure")||(graphType == "flow"&& friendly == "Flow")(graphType=="press2" && friendly == "Pressure2")||(graphType=="bpress" && friendly == "Barometric Pressure")||(graphType=="curr" && friendly == "Current")||(graphType=="humid" && friendly == "Humidity")){
+					if(graphType == "all"||(graphType=="temper" && friendly == "Pump Temperature")||(graphType=="press" && friendly == "Pressure")||(graphType == "flow"&& friendly == "Flow")||(graphType=="press2" && friendly == "Pressure2")||(graphType=="bpress" && friendly == "Barometric Pressure")||(graphType=="curr" && friendly == "Current")||(graphType=="humid" && friendly == "Humidity")){
 						
 						if (data.length > 0) {
 							last_val = data[data.length-1];
@@ -128,12 +126,14 @@ $(function() {
 								units: units
 							});
 							
+							changeCurrentValue(last_val[1],friendly);
+							
 						}
 					}
+				}
 				$("#placeholder").text('');
 				$.plot("#placeholder", data_to_plot, graph_options);
-				$("#appconsole").text('Data Plotted');
-				$("#appconsole").css('color', '#555555');
+			
 			}
 			
 			if (updateInterval != 0){
@@ -143,7 +143,6 @@ $(function() {
 
         function onError( jqXHR, textStatus, errorThrown) {
 			console.log('error: ' + textStatus + ',' + errorThrown);
-			$("#appconsole").text('No Server Response');
 			$("#appstatus").text('Server Offline');
 			$("#appstatus").css('color', red_color);
 			if (updateInterval != 0){
@@ -169,7 +168,62 @@ $(function() {
         });
 
 	}
+
+
+	document.getElementById("graphButton").addEventListener("click", addGraph);
+   
+	$("#remove2").click( function() {
+		document.getElementById("sleeperDiv2").style.visibility = "hidden"
+		index = 1;
+	});
 	
+	$("#remove3").click( function() {
+		document.getElementById("sleeperDiv3").style.visibility = "hidden";
+		index = 2;
+		if(document.getElementById("sleeperDiv2").style.visibility == "hidden"){
+			index = 1;
+		}
+	});
+	
+
+    
+	function changeCurrentValue(valueChange, valueColumn){
+		
+		if (valueColumn == "Pump Temperature"){
+			document.getElementById("currTemp").innerHTML= valueChange;
+			
+		}else if(valueColumn == "Pressure"){
+			document.getElementById("currPres").innerHTML= valueChange;
+			
+		}else if(valueColumn == "Flow"){
+			document.getElementById("currFlow").innerHTML= valueChange;
+		}
+		else if(valueColumn == "Pressure2"){
+			document.getElementById("currPres2").innerHTML= valueChange;
+			
+		}else if(valueColumn == "Humidity"){
+			document.getElementById("currHumid").innerHTML= valueChange;
+			
+		}else if(valueColumn == "Current"){
+			document.getElementById("currCurr").innerHTML= valueChange;
+			
+		}else if(valueColumn == "Barometric Pressure"){
+			document.getElementById("currApres").innerHTML= valueChange;
+			
+		}
+	}
+	
+	function addGraph(){
+		if(index >= 3){
+			alert("Only 3 graphs allowed");
+		}else{
+			index++;
+			var sleepy = "sleeperDiv" + index;
+			//alert(sleepy);
+			document.getElementById(sleepy).style.visibility = "visiblie";
+			
+		}
+	}
 	
 	$("#graphPick").val(graphPick).change(function () {
 		selectedValue = $("#graphPick").val();
@@ -189,7 +243,7 @@ $(function() {
 			graphType="bpress";
 		}else if(selectedValue == "current"){
 			graphType="curr";
-		}
+		}	
 	});
 
 	// Set up the control widget
@@ -197,8 +251,9 @@ $(function() {
 	$("#updateInterval").val(updateInterval).change(function () {
 		var v = $(this).val();
 		if (v && !isNaN(+v)) {
-			if(updateInterval == 0)
-				{setTimeout(fetchData, 1000);} //updates were turned off, start again
+			if(updateInterval == 0){
+				setTimeout(fetchData, 1000);
+				} //updates were turned off, start again
 			updateInterval = +v;
 			if (updateInterval > 20000) {
 				updateInterval = 20000;
